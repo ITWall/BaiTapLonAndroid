663b0fc07500bb1bb941db9233bca26ddd112d61
nguyen.thi.thu.thuy <thuthuynt.0710@gmail.com>
2018-05-25 01:43:48 +0700

    test

--- app/src/main/java/ntt/thuy/com/btlmusicplayer/online/OnlineFragment.java
+++ app/src/main/java/ntt/thuy/com/btlmusicplayer/online/OnlineFragment.java
@@ -186,32 +186,8 @@ public class OnlineFragment extends Fragment implements OnGetAllTracks, TrackLis
             }
         });
 
-//        setupMediaPlayer();
     }
 
-//    private void setupMediaPlayer(){
-//        mMediaPlayer = new MediaPlayer();
-//        mMediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
-//        mMediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
-//            @Override
-//            public void onPrepared(MediaPlayer mp) {
-//                progress.dismiss();
-//
-//                anim = AnimationUtils.loadAnimation(getContext(), R.anim.rotate);
-//                mSelectedTrackImage.startAnimation(anim);
-//
-//                togglePlayPause();
-//            }
-//        });
-//    }
-//
-//    private void togglePlayPause() {
-//        if (mMediaPlayer.isPlaying()) {
-//            mMediaPlayer.pause();
-//        } else {
-//            mMediaPlayer.start();
-//        }
-//    }
 
     private void showBottomBar(Track track) {
         seekBar.setVisibility(View.VISIBLE);
